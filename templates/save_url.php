b0b5fa75fb5790b8db34d86f00e265da49d23f82
Brandon Telle <brandon.telle@lootcrate.com>
2016-03-01 15:46:31 -0800

    Add missing save_url.php

---
+++ templates/save_url.php
@@ -0,0 +1,21 @@
+<div class="container router default-off" id="route-saver">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <h1>Save an Image</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <form>
+                <div class="form-group">
+                    <label class="control-label" for="image-url">Image URL</label>
+                    <input type="text" id="image-url" class="form-control" value="" placeholder="https://farmX.flickr.com/..._o.jpg">
+                </div>
+                <div class="help-block">
+                    <small>Note: this file will only be saved to the saved directory. It will not appear in the saved images view.</small>
+                </div>
+                <button type="submit" class="btn btn-primary">Save Image</button>
+            </form>
+        </div>
+    </div>
+</div>
